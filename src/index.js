4b3b3afd8b36498c8427d238ceea5b4b8178d28a
Victor Rocha <victor.rocha2002@gmail.com>
2023-11-16 20:52:21 -0300

    mensagens

--- src/index.js
+++ src/index.js
@@ -8,10 +8,17 @@ const app = express();
 app.use(express.json());
 
 app.get('/', async(req, res)=>{
-    return res.json('API OK CYCLIC!');
+    return res.json('Oi nenem...infelizmento nao sou tão foda igual o cara do tiktok(até pq não sei a área dele...Mas tentei user um pouco do que eu sei...Vai digitando: 1,2 e 3 na barra ali em cima na URL');
 })
-
-
+app.get('/1', async(req, res)=>{
+    return res.json('Oi amo, queria pedir desculpa de nao ter tirado meu tempo antes pra ficar com vc... e fazer vc perder seu tempo...');
+})
+app.get('/2', async(req, res)=>{
+    return res.json('Entao tirei um tempinho agora pra te monstrar as nerdolices que eu to aprendendo');
+})
+app.get('/3', async(req, res)=>{
+    return res.json('Te amo, e me desculpa');
+})
 app.listen(PORT, ()=>{
     console.log(PORT);
 });
